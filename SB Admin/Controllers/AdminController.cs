6b58084f5b4ebd4339fe244e72771c9f004c18f5
suleyman <suleyman_canbaz@outlook.com>
2026-02-04 00:08:09 +0300

    Admin paneli ve blog için ayrı layout yapısı eklendi
    Admin paneline özel `_AdminLayout.cshtml` ve `AdminController` eklendi. Admin paneli sayfaları bu layout'a taşındı. Ana layout dosyası blog temasıyla değiştirildi. Blog ana sayfası ve örnek içerik eklendi. Favicon güncellendi. Uygulama, blog ve admin paneli olarak ikiye ayrıldı.

---
+++ SB Admin/Controllers/AdminController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SB_Admin.Models;
+using System.Diagnostics;
+
+namespace SB_Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult layout_sidenav_light()
+        {
+            ViewData["SideNavClass"] = "sb-sidenav-light";
+            return View("Index");
+        }
+
+        public IActionResult layout_static()
+        {
+            ViewData["SideNavClass"] = "sb-sidenav-dark";
+            return View("Index");
+        }
+
+        public IActionResult Charts()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Password()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Tables()
+        {
+            return View();
+        }
+
+        public IActionResult Error500()
+        {
+            return View();
+        }
+        public IActionResult Error404()
+        {
+            return View();
+        }
+
+        public IActionResult Error401()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
